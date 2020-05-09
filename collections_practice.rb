edd7a102920d850e925d07e3a5faf68005675512
Jimmy Millard <millardjimmy@gmail.com>
2020-05-09 15:44:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,3 +1,43 @@
-def sort_array_asc(array)
-  return array.sort 
+#question 1
+def sort_array_asc (array)
+  array.sort
+end
+
+
+#question 2 
+def sort_array_desc (array)
+  array.sort {|x,y| y <=> x }
+end
+
+#question 3
+def sort_array_char_count
 end 
+
+#question 4
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+
+#question 5
+def reverse_array (array)
+  array.reverse
+end
+
+
+#question 6
+def kesha_maker (array)
+  array.collect do |element|
+    element[2] = "$"
+    element
+  end
+end
+
+#question 7 find_a
+
+#question 8 sum_array
+
+#question 9 add_s
+
+
